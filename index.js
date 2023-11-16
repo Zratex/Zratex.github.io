cbd76f24c239b70a63c82ec6e76c4446808f27ec
foul0028 <bryan.fouliard@etudiant.univ-reims.fr>
2023-11-16 14:03:02 +0100

    Test fond acceuil Véronica

---
+++ index.js
@@ -0,0 +1,10 @@
+// Fonctions lanceant les vidéos :
+function VeronicaPlayVideo(){
+    var veronicaVideo = document.getElementById("veronicavideo");
+    veronicaVideo.play();
+}
+
+function VeronicaPauseVideo(){
+    var veronicaVideo = document.getElementById("veronicavideo");
+    veronicaVideo.pause();
+}
