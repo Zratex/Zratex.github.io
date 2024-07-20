efb183491ae56464db9160eec4c922edf945a421
foul0028 <bryan.fouliard@etudiant.univ-reims.fr>
2024-07-20 10:30:02 +0200

    Fichier de déploiement

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/zratex.github.io/',
+  base: '/', // Base path for GitHub Pages at the root level
   build: {
     outDir: 'dist'
   }
