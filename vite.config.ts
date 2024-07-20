19879a7920522c1b091ab9ab5724378afa48855f
foul0028 <bryan.fouliard@etudiant.univ-reims.fr>
2024-07-20 10:12:40 +0200

    Mauvaise URL, giga mb

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Zratex.githu.io/',
+  base: '/zratex.github.io/',
   build: {
     outDir: 'dist'
   }
