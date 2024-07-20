c45ad471db3b8a27b79e0532d988c5fabd0cdd0e
foul0028 <bryan.fouliard@etudiant.univ-reims.fr>
2024-07-20 21:22:20 +0200

    Maj 404

--- src/main.tsx
+++ src/main.tsx
@@ -2,27 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { HashRouter, Route,  Routes } from 'react-router-dom';
 import { Gallery } from './pages/Gallery.tsx';
 import { Projects } from './pages/Projects.tsx';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />
-  },
-  {
-    path: "/projects",
-    element: <Projects />
-  },
-  {
-    path: "/gallery",
-    element: <Gallery />
-  }
-]);
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/gallery" element={<Gallery />} />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>,
 )
