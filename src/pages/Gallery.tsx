30b258f84ac2f38d823c6d89df20e0929dc33060
foul0028 <bryan.fouliard@etudiant.univ-reims.fr>
2024-07-23 18:36:26 +0200

    Pause manger

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -50,6 +50,42 @@ function Gallery() {
                     toggleOverallModal={toggleOverallModal}
                 ></Card>
                 <Card
+                    title="[TBG] Underground Arena: New Generation - Affiches"
+                    description={["En plus d'être président, je suis aussi le graphiste de l'association The Battle Ground. A l'occasion de notre premier tournoi sur le jeu 'Super Smash Bros. Ultimate' (2018 Nintendo, Sora | Bandai Namco), j'ai réalisé un clip vidéo inspiré de la cinématique d'introduction de 'Street Fighter III: New Generation' (1997 Capcom).",
+                        "Chacune des frames ont été extraites pour en faire des affiches à part. Dans cette collection d'images que vous pouvez visionner, vous aurez le rendu après quelques retouches, suivi de l'affiche en elle même.",
+                        "Dans le lien ci dessous se trouvera une comparaison entre la vidéo que j'ai réalisé, et la cinématique d'origine."
+                    ]}
+                    imagelink={[
+                        "https://raw.githubusercontent.com/Zratex/gallery/main/TBG/Affiches%20TBG/UAnew/Roy%20shadered%20w%20background.png",
+                        "https://raw.githubusercontent.com/Zratex/gallery/main/TBG/Affiches%20TBG/UAnew/Roy.png",
+                        "https://raw.githubusercontent.com/Zratex/gallery/main/TBG/Affiches%20TBG/UAnew/Pyra%20%26%20Mythra%20shaded%20w%20background.png",
+                        "https://raw.githubusercontent.com/Zratex/gallery/main/TBG/Affiches%20TBG/UAnew/Aegis.png",
+                        "https://raw.githubusercontent.com/Zratex/gallery/main/TBG/Affiches%20TBG/UAnew/Cloud%20shaded%20w%20background.png",
+                        "https://raw.githubusercontent.com/Zratex/gallery/main/TBG/Affiches%20TBG/UAnew/Cloud.png",
+                        "https://raw.githubusercontent.com/Zratex/gallery/main/TBG/Affiches%20TBG/UAnew/Falcon%20shaded%20w%20background.png",
+                        "https://raw.githubusercontent.com/Zratex/gallery/main/TBG/Affiches%20TBG/UAnew/Falcon.png",
+                        "https://raw.githubusercontent.com/Zratex/gallery/main/TBG/Affiches%20TBG/UAnew/Fox%20shaded%20w%20background.png",
+                        "https://raw.githubusercontent.com/Zratex/gallery/main/TBG/Affiches%20TBG/UAnew/Fox.png",
+                        "https://raw.githubusercontent.com/Zratex/gallery/main/TBG/Affiches%20TBG/UAnew/Incineroar.png",
+                        "https://raw.githubusercontent.com/Zratex/gallery/main/TBG/Affiches%20TBG/UAnew/LM.png",
+                        "https://raw.githubusercontent.com/Zratex/gallery/main/TBG/Affiches%20TBG/UAnew/Mario%20shaded%20w%20background.png",
+                        "https://raw.githubusercontent.com/Zratex/gallery/main/TBG/Affiches%20TBG/UAnew/Mario.png",
+                        "https://raw.githubusercontent.com/Zratex/gallery/main/TBG/Affiches%20TBG/UAnew/Puff%20shaded%20w%20background.png",
+                        "https://raw.githubusercontent.com/Zratex/gallery/main/TBG/Affiches%20TBG/UAnew/Puff.png",
+                        "https://raw.githubusercontent.com/Zratex/gallery/main/TBG/Affiches%20TBG/UAnew/Sephiroth%20shaded%20w%20shadow.png",
+                        "https://raw.githubusercontent.com/Zratex/gallery/main/TBG/Affiches%20TBG/UAnew/Sephiroth.png",
+                        "https://raw.githubusercontent.com/Zratex/gallery/main/TBG/Affiches%20TBG/UAnew/Zelda%20shaded%20w%20background.png",
+                        "https://raw.githubusercontent.com/Zratex/gallery/main/TBG/Affiches%20TBG/UAnew/Zelda.png",
+                    ]}
+                    redirection_link="https://youtu.be/XXY-FQMnk6M"
+                    backgroundcolor={itemBackgroundColor}
+                    bordercolor={itemBorderColor}
+                    tags={["Blender"]}
+                    popup={true}
+                    overallModalState={overallModal}
+                    toggleOverallModal={toggleOverallModal}
+                ></Card>
+                <Card
                     title="Neon Slide"
                     description={["Rendu d'une scène 3D sous Blender comprenant la map 'Haven' de Valorant (2020 Riot Games), ainsi que du modèle 3D et d'animation de sélection de Neon qui est un personnage tirée du même jeu",
                         "J'ai donc seulement faire le retexturing du personnage, la composition de tout ces éléments, les effets visuels, et le mouvement de camera.",
